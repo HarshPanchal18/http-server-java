8f140abf21c863d71750908deb87d10fdcaa0377
Harsh Panchal <harshhhh1803@gmail.com>
2024-06-28 16:08:31 +0530

    Fix HttpRequest bug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,25 +20,24 @@ public class Main {
 
             InputStream input = clientSocket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-            String line = reader.readLine();
-            String[] HttpRequest = line.split(" ", 0);
 
             OutputStream output = clientSocket.getOutputStream();
 
             String request;
-            ArrayList<String> HttpReq = new ArrayList<>();
+            ArrayList<String> HttpRequest = new ArrayList<>();
             // read request completely HTTP requests don't end with EOF but with blank line.
             while (!(request = reader.readLine()).isEmpty())
-                HttpReq.add(request);
+                HttpRequest.add(request);
 
-            System.out.println(HttpReq);
+            System.out.println(HttpRequest);
 
-            // Striping URL from the HTTP req
-            String[] URL = HttpReq.get(0).split(" ", 0);
+            // Striping URL from the HTTP request
+            String[] URL = HttpRequest.get(0).split(" ", 0);
 
-            if (HttpRequest[1].equals("/")) {
+            if (HttpRequest.get(1).equals("/")) {
                 output.write(("HTTP/1.1 200 OK\r\n\r\n".getBytes()));
-            } else if (HttpRequest[1].startsWith("/echo/")) {
+
+            } else if (HttpRequest.get(1).startsWith("/echo/")) {
                 String[] path = URL[1].split("/");
                 String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" +
                         path[2].length() + "\r\n\r\n" + path[2];
@@ -54,9 +53,11 @@ public class Main {
                 String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" +
                         userAgent[1].length() + "\r\n\r\n" + userAgent[1];
                 output.write(response.getBytes());
+
             } else {
                 output.write(("HTTP/1.1 404 Not Found\r\n\r\n".getBytes()));
             }
+
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
