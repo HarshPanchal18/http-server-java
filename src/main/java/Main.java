4df46d3c1f5b1b2509a3c5d4f32c6ea5eca142a1
Harsh Panchal <harshhhh1803@gmail.com>
2024-06-28 16:02:39 +0530

    Fix bug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,14 +25,26 @@ public class Main {
 
             OutputStream output = clientSocket.getOutputStream();
 
+            String request;
+            ArrayList<String> HttpReq = new ArrayList<>();
+            // read request completely HTTP requests don't end with EOF but with blank line.
+            while (!(request = reader.readLine()).isEmpty())
+                HttpReq.add(request);
+
+            System.out.println(HttpReq);
+
+            // Striping URL from the HTTP req
+            String[] URL = HttpReq.get(0).split(" ", 0);
+
             if (HttpRequest[1].equals("/")) {
                 output.write(("HTTP/1.1 200 OK\r\n\r\n".getBytes()));
             } else if (HttpRequest[1].startsWith("/echo/")) {
-                String message = HttpRequest[1].substring(6);
-                String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s" +
-                        message.length() + message;
+                String[] path = URL[1].split("/");
+                String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" +
+                        path[2].length() + "\r\n\r\n" + path[2];
                 output.write(response.getBytes());
-            } else if (HttpRequest[1].equals("/user-agent")) {
+
+            } else if (URL[1].startsWith("/user-agent")) {
                 String[] userAgent = new String[2];
                 for (String s : HttpRequest) {
                     if (s.startsWith("User-Agent"))
