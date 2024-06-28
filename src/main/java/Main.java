04129b27f72d9e09ca556601e9b7b2b63f114d3b
Harsh Panchal <harshhhh1803@gmail.com>
2024-06-28 19:39:37 +0530

    Reverting changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,15 +9,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
-        String CRLF = "\r\n";
-
         String directory = "";
         if (args.length > 1 && args[0].equals("--directory")) {
             directory = args[1];
-            System.out.println(directory);
+            System.out.println(args[1]);
         }
 
         try {
@@ -26,35 +23,36 @@ public class Main {
 
             while (true) {
                 clientSocket = serverSocket.accept(); // Wait for connection from client.
-                // client side conversion of bytes into data.
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                ArrayList<String> HttpRequest = getHttpRequest(clientSocket);
+                //client side conversion of bytes into data.
+                BufferedReader clientIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                OutputStream outputStream = clientSocket.getOutputStream();
+                String request; // Read the request
+                ArrayList<String> HttpRequest = new ArrayList<>();
+
+                //read request completely HTTP requests don't end with EOF but with blank line.
+                while (!(request = clientIn.readLine()).equals(""))
+                    HttpRequest.add(request);
 
                 System.out.println(HttpRequest);
-
-                OutputStream outputStream = clientSocket.getOutputStream();
-
-                // Striping URL from the HTTP request
+                //Striping URL from the HTTP req
                 String[] URL = HttpRequest.get(0).split(" ", 0);
                 if (URL[0].equals("POST")) {
                     StringBuffer data = new StringBuffer();
-                    while (bufferedReader.ready())
-                        data.append((char) bufferedReader.read());
+                    while (clientIn.ready())
+                        data.append((char) clientIn.read());
 
                     String body = data.toString();
                     Path path = Paths.get(directory, URL[1].split("/")[2]);
                     Files.write(path, body.getBytes());
-                    String response = "HTTP/1.1 201 Created" + CRLF + CRLF;
-                    outputStream.write(response.getBytes(StandardCharsets.UTF_8));
-
+                    outputStream.write("HTTP/1.1 201 Created\r\n\r\n".getBytes(StandardCharsets.UTF_8));
                 } else {
                     if (URL[1].equals("/")) {
-                        String response = "HTTP/1.1 200 OK" + CRLF + CRLF;
+                        String response = "HTTP/1.1 200 OK\r\n\r\n";
                         outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+
                     } else if (URL[1].startsWith("/echo/")) {
                         String[] path = URL[1].split("/", 0);
-                        String response = "HTTP/1.1 200 OK" + CRLF + "Content-Type: text/plain" + CRLF + "Content-Length:" +
-                                path[2].length() + CRLF + CRLF + path[2];
+                        String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + path[2].length() + "\r\n\r\n" + path[2];
                         outputStream.write(response.getBytes());
                     } else if (URL[1].startsWith("/user-agent")) {
                         String[] userAgent = new String[2];
@@ -62,25 +60,24 @@ public class Main {
                             if (s.startsWith("User-Agent"))
                                 userAgent = s.split(": ");
 
-                        String response = "HTTP/1.1 200 OK" + CRLF + "Content-Type: text/plain" + CRLF + "Content-Length:" +
-                                userAgent[1].length() + CRLF + CRLF + userAgent[1];
+                        String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + userAgent[1].length() + "\r\n\r\n" + userAgent[1];
                         outputStream.write(response.getBytes());
                     } else if (URL[1].startsWith("/files")) {
                         String filename = URL[1].split("/", 0)[2];
                         File file = new File(directory, filename);
                         System.out.println(file.toPath());
-
                         if (file.exists()) {
+                            //reading byte content
                             byte[] fileContent = Files.readAllBytes(file.toPath());
-                            String response = "HTTP/1.1 200 OK" + CRLF + "Content-Type: application/octet-stream" + CRLF +
-                                    "Content-Length: " + fileContent.length + CRLF + CRLF + new String(fileContent);
-                            outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+                            String httpResponse = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " +
+                                            fileContent.length + "\r\n\r\n" + new String(fileContent);
+                            outputStream.write(httpResponse.getBytes(StandardCharsets.UTF_8));
                         } else {
-                            String response = "HTTP/1.1 404 Not Found" + CRLF + CRLF;
+                            String response = "HTTP/1.1 404 Not Found\r\n\r\n";
                             outputStream.write(response.getBytes());
                         }
                     } else {
-                        String response = "HTTP/1.1 404 Not Found" + CRLF + CRLF;
+                        String response = "HTTP/1.1 404 Not Found\r\n\r\n";
                         outputStream.write(response.getBytes());
                     }
                     clientSocket.close();
@@ -90,19 +87,5 @@ public class Main {
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
-    }
-
-    private static ArrayList<String> getHttpRequest(Socket clientSocket) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        // Read the request
-        String request;
-        ArrayList<String> HttpRequest = new ArrayList<>();
-        // read request completely HTTP requests don't end with EOF but with blank line.
-        while (!(request = bufferedReader.readLine()).equals(""))
-            HttpRequest.add(request);
-
-        bufferedReader.close();
-
-        return HttpRequest;
     }
 }
