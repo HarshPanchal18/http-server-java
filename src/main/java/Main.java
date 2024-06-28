e834a6034cebb6155b644822dc118ffedb641fae
Harsh Panchal <harshhhh1803@gmail.com>
2024-06-28 15:01:42 +0530

    Responding with 200 or 400 code, depending on path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,36 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-     ServerSocket serverSocket = null;
-     Socket clientSocket = null;
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
 
-     try {
-       serverSocket = new ServerSocket(4221);
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+        try {
+            serverSocket = new ServerSocket(4221);
+            serverSocket.setReuseAddress(true);
+
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            clientSocket = serverSocket.accept(); // Wait for connection from client.
+
+            InputStream input = clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            String line = reader.readLine();
+            String[] HttpRequest = line.split(" ", 0);
+
+            OutputStream output = clientSocket.getOutputStream();
+
+            if (HttpRequest[1].equals("/")) {
+                output.write(("HTTP/1.1 200 OK\r\n\r\n".getBytes()));
+            } else {
+                output.write(("HTTP/1.1 404 Not Found\r\n\r\n".getBytes()));
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
